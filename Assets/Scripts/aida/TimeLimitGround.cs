9ffe41d3c30591b69e3b9c18c4948aada66a3f5e
maya0117 <mynhjm@icloud.com>
2023-11-16 22:38:26 +0900

    制限時間付きの足場の処理。 修正

--- Assets/Scripts/aida/TimeLimitGround.cs
+++ Assets/Scripts/aida/TimeLimitGround.cs
@@ -5,65 +5,117 @@ using Unity.VisualScripting;
 using UnityEngine;
 using UnityEngine.UI;
 
-//制限時間付きの足場の処理。一定時間が過ぎると足場が崩れる
+//制限時間付きの足場の処理。
 public class TimeLimitGround : MonoBehaviour
 {
-    [SerializeField]
-    private SpriteRenderer beforeImage = null;
-    [SerializeField]
-    private SpriteRenderer afterImage = null;
-    private SpriteRenderer currentImage = null;
-    //足場が崩れるまでの時間
-    [SerializeField]
-    private float timeLimit = 5f;
-    //プレイヤーが足場に乗っている時間
-    private float totalTime = 0f;
-    private int flag = 0;
-    private Collider2D col;
-    private PlayerEnvroment _env;
-
-    private void Start()
+    enum State 
     {
-        currentImage = GetComponent<SpriteRenderer>();
-        currentImage.color = beforeImage.color;
-        col = GetComponent<Collider2D>();
+        Wait,
+        Init,
+        Corpse,
+        Reactive,
     }
 
-    public void SetUp(PlayerEnvroment env, CancellationToken token)
+    //崩れる前のスプライト
+    [SerializeField] private SpriteRenderer beforeImage = null;
+    //崩れた後のスプライト
+    [SerializeField] private SpriteRenderer afterImage = null;
+    //現在のスプライト
+  　private SpriteRenderer currentImage = null;
+    //足場が崩れるまでの時間
+    [SerializeField] private float timeLimit = 5f;
+    //プレイヤーが足場に乗っている時間
+    private float totalTime = 0f;
+    private State state = State.Wait;
+    private Collider2D col;
+    [SerializeField] private PlayerEnvroment _env;
+
+    public void Start()
     {
-        _env = env;
+        state = State.Wait;
+        currentImage = GetComponent<SpriteRenderer>();
+        col = GetComponent<Collider2D>();
+        currentImage.sprite = beforeImage.sprite;
     }
 
     private void Update()
     {
-        Debug.DrawLine(transform.position + Vector3.up * 0.5f, transform.position + Vector3.up * -0.2f, Color.red);
-        if (_env.PlayerTransform.position.y == gameObject.transform.position.y + 0.5f)
-        {
-            flag = 1;
-            Debug.Log("Col");
-        }
-
-        if (flag != 0)
-        {
-            ReceiveForce();
-
-            if (totalTime >= timeLimit)
-            {
-                totalTime = 0f;
-                col.gameObject.SetActive(false);
-                currentImage.color = afterImage.color;
+        switch (state) {
+            case State.Wait:
+                Debug.Log("Wait");
+                break;
+            case State.Init:
+                Debug.Log("Init");
+                totalTime += Time.deltaTime;
+                if (ReceiveForce())
+                {
+                    totalTime = 0;
+                    state = State.Corpse;
+                }
+                break;
+            case State.Corpse:
                 Debug.Log("Corpse");
-            }
+                Corpse();
+                if (ReceiveForce())
+                {
+                    totalTime = 0;
+                    state = State.Reactive;
+                }
+                break;
+            case State.Reactive:
+                Debug.Log("Reactive");
+                Reactive();
+                if (ReceiveForce())
+                {
+                    totalTime = 0;
+                    state = State.Wait;
+                }
+                break;
         }
-        else
-        {
-            totalTime = 0f;
-        }
+
     }
 
-    public void ReceiveForce()
+    private bool ReceiveForce()
     {
         totalTime += Time.deltaTime;
+        if (timeLimit <= totalTime)
+        {
+            return true;
+        }
+        return false;
     }
 
+
+    private void Reactive()
+    {
+        currentImage.sprite = beforeImage.sprite;
+        col.enabled = true;
+        Debug.Log("Reactive");
+    }
+
+    private void Corpse()
+    {
+        currentImage.sprite = afterImage.sprite;
+        col.enabled = false;
+        Debug.Log("Corpse");
+    }
+
+    private void OnCollisionEnter2D(Collision2D collision)
+    {
+        if(collision.gameObject.name == "Player")
+        {
+            state = State.Init;
+            Debug.Log("Enter");
+        }
+    }
+
+    private void OnCollisionExit2D(Collision2D collision)
+    {
+        if(collision.gameObject.name == "Player" && col.enabled)
+        {
+            totalTime = 0;
+            state = State.Wait;
+            Debug.Log("Exit");
+        }
+    }
 }
