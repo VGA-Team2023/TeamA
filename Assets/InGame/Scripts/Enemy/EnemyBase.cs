fc7258971e6cff6cdfc5f04698dcfee3d033c856
結城 黎 <106291652+YukiR0708@users.noreply.github.com>
2023-11-20 05:40:35 +0900

    [Add]新しいEnemyBaseを作成

---
+++ Assets/InGame/Scripts/Enemy/EnemyBase.cs
@@ -0,0 +1,42 @@
+using UnityEngine;
+
+/// <summary> Enemy（ザコとボス）共通の基底クラス </summary>
+public abstract class EnemyBase : MonoBehaviour, IReceiveWater
+{
+    [SerializeField, Tooltip("Playerとの距離")]
+    float _distance = 0f;
+    public float Distance => _distance;
+
+    private void Update()
+    {
+        if (GameManager.Instance != null)
+        {
+            _distance = MeasureDistance();
+        }
+    }
+
+    /// <summary> 水があたったときに呼ばれる。 </summary>
+    public void ReceiveWater()
+    {
+        Damaged();
+    }
+
+    /// <summary> 攻撃時に呼ぶメソッド </summary>
+    public abstract void Attack();
+    /// <summary> 移動時に呼ぶメソッド </summary>
+    public abstract void Move();
+    /// <summary> 被ダメ時に呼ぶメソッド </summary>
+    public abstract void Damaged();
+    /// <summary> 退場時に呼ぶメソッド </summary>
+    public abstract void Exit();
+
+    /// <summary> Playerとの距離を測る </summary>
+    /// <returns>Playerとの距離</returns>
+    public float MeasureDistance()
+    {
+        Vector2 pPos = GameManager.Instance.PlayerEnvroment.PlayerTransform.position;   //Playerの座標
+        float distance = Vector2.Distance(this.transform.position, pPos);   //Playerとの距離
+        return distance;
+    }
+
+}
